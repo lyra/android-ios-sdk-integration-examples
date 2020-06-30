9d665298d5c95b73139cfa2913360a5073ff4bac
Soler Alexandre <alexandre.soler@lyra-network.com>
2020-06-30 15:12:50 +0200

    MOBP-735: Goto AndroidX

--- android/java/app/src/main/java/com/lyra/sdk/sample/java/MainActivity.java
+++ android/java/app/src/main/java/com/lyra/sdk/sample/java/MainActivity.java
@@ -1,10 +1,11 @@
 package com.lyra.sdk.sample.java;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
